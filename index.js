901dc1d36c18ef9f5e50c733686e1b6e4d8988c8
michaels2533 <michael.stawowski@wsu.edu>
2024-10-10 12:19:50 -0400

    Fixed and completed problem 2

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const reverseZigZagString = (str) => {
   for (let i = str.length; i >= 1; i--) {
     if (i % 2 === 0) {
       reversed += str[i - 1];
-      console.log(`${i}`);
     } else {
       reversed += str[i - 1].toUpperCase();
     }
@@ -24,5 +23,5 @@ const reverseZigZagString = (str) => {
 
 const reversedString1 = reversedString('hello');
 console.log('Problem 1  reversedString1', reversedString1);
-const reverseZigZagString1 = reverseZigZagString('hello');
+const reverseZigZagString1 = reverseZigZagString('helloo');
 console.log('Problem 2 reverseZigZagString1', reverseZigZagString1);
